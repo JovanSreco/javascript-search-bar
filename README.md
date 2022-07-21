c73c41da948ded63467b69214aed2d23dd1bb8d8
Jovan Sreco <jovan.sreco@gmail.com>
2022-07-21 22:45:17 +0200

    Version 1.0.0

---
+++ README.md
@@ -0,0 +1,10 @@
+# Javascript Tic Tac Toe
+
+This is a **Search Bar** project that lets search for employees in the input HTML element, the search input will display all cards of employee information that look for Name Email, and City(where they live). 
+You can click the **View More Info** button for more information about every employee.
+
+## Tools used
+[Fetch API](https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API), [Map](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Map), [JSON placeholder](https://jsonplaceholder.typicode.com/), and lost of [Destructuring](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment).
+
+## Files
+Run command npm install in terminal if you wish to experiment with Sass
--- script.js
+++ script.js
@@ -1,44 +1,66 @@
 const employeeCardContainer = document.querySelector("[data-employee-card-container]")
 const employeeCardTemplate = document.querySelector("[data-employee-card-template]")
 const searchInput = document.querySelector("[data-search]")
-const viewMoreInfoBtns = document.querySelectorAll("[data-employee-card-btn]")
 const popupInfo = document.querySelector("[data-popup-info]")
+const popupInfoItems = document.querySelectorAll("[data-popup-info-item]")
 const popupBtn = document.querySelector("[data-info-popup-btn]")
-const popupItemsContainer = document.querySelector("data-ul-data-popup-info-item")
-const popupItems = document.querySelectorAll("[data-popup-info-item]")
 const employees = new Map()
+
+// The main search for employees. Searches Name Email and City(where they live)
+searchInput.addEventListener("input", e => {
+    const value = e.target.value.toLowerCase();
+
+    employees.forEach((obj, element) => {
+        let {name, email, city} = obj
+        const visible = name.toLowerCase().includes(value) || email.toLowerCase().includes(value) || city.toLowerCase().includes(value)
+        element.classList.toggle("employees-card--invisible", !visible)
+    })
+})
 
 // Fetch random data about some hypothetical employees then store everything in employees Map
 fetch("https://jsonplaceholder.typicode.com/users")
     .then(response => response.json())
     .then(data => {
         data.forEach( (employee) => {
-            // Using destructuring to practice, and Map() to store our data for each employee
-            let {id, name, username, email, address: { street: street, suite: suite, city: 
+            // Using destructuring, and Map() to store our data for each employee
+            const {id, name, username, email, address: { street: street, suite: suite, city: 
                 city, zipcode: zipcode, geo: {lat: lat, lng: lng}}} = employee
-            employees.set(employee.name.toLowerCase(), {id:id,name:name, username:username, email:email, 
-                street:street, suite:suite, city:city, zipcode:zipcode, lat:lat, lng:lng})
-            // Have to append info to cards during each iteration
-            appendCardInfo(name, email, city)
+            //Creating a clone of a Node and setting it as a key in Map and value is an object of all information
+            const card = employeeCardTemplate.content.cloneNode(true).children[0]
+            employees.set(card, {id:id,name:name, username:username, email:email, street:street, 
+                          suite:suite, city:city, zipcode:zipcode, lat:lat, lng:lng})
+            // Have to append info to cards during each iteration. Each Card VIEW MORE BTN has to have an event listener for getting more information
+            appendInformation(name, email, city, card)
         })
     })
 
-function appendCardInfo(...args){
-    const card = employeeCardTemplate.content.cloneNode(true).children[0]
+function appendInformation(...args){
+    //Last element of arguments is card element
+    const card = args[args.length - 1]
     const cardInfo = card.querySelectorAll("[data-employee-card-info]")
     const cardBtn = card.querySelector("[data-employee-card-btn]")
+    // Basic card info 
+    args.forEach((item, index) => {
+        if(index !== args.length - 1) cardInfo[index].textContent = cardInfo[index].textContent + item;
+    })
+    employeeCardContainer.appendChild(card)
+    // Add listener
+    listenCardBtn(card, cardBtn)
+}
+
+function listenCardBtn(card, cardBtn) {
+    //Logic for card buttons to display more information in the popup element
     cardBtn.addEventListener("click", () => {
         popupInfo.classList.add("active");
-        handleMoreInfo(args[0])
-    })
-    args.forEach((item, index) => {
-        cardInfo[index].textContent = cardInfo[index].textContent + item
-        employeeCardContainer.appendChild(card)
+        const obj = employees.get(card)
+        const arrayOfItems = [...popupInfoItems]
+        arrayOfItems.forEach( (div, index) => {
+            let key = Object.keys(obj)[index]
+            key = key.slice(0, 1).toUpperCase() + key.slice(1)
+            const value = Object.values(obj)[index]
+            div.textContent = `${key}: ${value}`
+        });
     })
 }
 
-
-
-popupBtn.addEventListener("click", () => {
-    popupInfo.classList.remove("active")
-})
+popupBtn.addEventListener("click", () => popupInfo.classList.remove("active"))
